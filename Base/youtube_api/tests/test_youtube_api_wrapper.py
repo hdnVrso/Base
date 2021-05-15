628fbe10a1c4363b5a8d97a271b6715c9d028444
boyboyjoy <moximmorozov@yandex.ru>
2021-05-15 18:05:06 +0430

    done 90 percent coverage youtube wrapper

--- Base/youtube_api/tests/test_youtube_api_wrapper.py
+++ Base/youtube_api/tests/test_youtube_api_wrapper.py
@@ -11,3 +11,13 @@ class YoutubeApiWrapperTestCase(TestCase):
     def test_wrapper_get_response(self):
         self.wrapper.search_youtube_channels_by_keyword(self.TEST_CHANNEL_TITLE)
         self.assertTrue(self.wrapper.youtube_channel_list_data)
+
+    def test_wrapper_correctly_parse_data(self):
+        channel_keys_list = ["channel_url", "channel_title",
+                             "channel_description", "channel_image",
+                             "subscriber_count", "video_count:"]
+        self.wrapper.search_youtube_channels_by_keyword(self.TEST_CHANNEL_TITLE)
+        channel_data = self.wrapper.get_channels_data()["channel 0"]
+        for key in channel_keys_list:
+            if key not in channel_data.keys():
+                self.assertTrue(False)
