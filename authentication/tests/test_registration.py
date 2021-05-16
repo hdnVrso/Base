66c99a0b570b9770d2aa01c26e2f27363cc2104a
Viktor Repin <hiddenstmail@gmail.com>
2021-05-16 12:35:21 +0700

    Fix token parser

--- authentication/tests/test_registration.py
+++ authentication/tests/test_registration.py
@@ -38,18 +38,22 @@ class RegistrationTests(APITestCase, URLPatternsTestCase, TransactionTestCase):
 
     def test_returns_200_ok_if_request_is_valid(self):
         url = reverse('authentication:register')
-        request = {'email': 'email@email.com', 'username': 'use1214',
-                   'password': 'Pass1234'}
+        request = {
+            'user': {'email': 'email@email.com', 'username': 'user12134',
+                     'password': 'Pass12344'}
+            }
         response = self.client.post(url, format='json', request=request)
         self.assertEqual(response.status_code,
                          status.HTTP_200_OK)
+        print(response.body)
 
     def test_returns_correct_response_body_if_request_is_valid(self):
         url = reverse('authentication:register')
         request = {'user': {'email': 'email@email.com', 'username': 'user1214',
-                   'password': 'Pass1234'}}
+                            'password': 'Pass1234'}
+                  }
         response = self.client.post(url, format='json', request=request)
-        self.assertEqual(response.data, {
-                             'email': request['email'],
-                             'username': request['username']
+        self.assertEqual(response.body, {
+                             'email': request['user']['email'],
+                             'username': request['user']['username']
         })
