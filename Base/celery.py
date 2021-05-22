f12800864988491608f7f5bb38eda1497d673e74
boyboyjoy <moximmorozov@yandex.ru>
2021-05-22 17:27:23 +0430

    add data taker for weeks

--- Base/celery.py
+++ Base/celery.py
@@ -15,7 +15,7 @@ app.autodiscover_tasks()
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
         5.0,
-        get_top.s(),
+        create_top_requests_per_week.s(),
     )
 
 
@@ -23,27 +23,50 @@ app.conf.timezone = 'UTC'
 
 
 @app.task
-def get_top():
+def create_top_requests_per_day():
     from authentication.models import RequestModel
     COUNT_OF_TOP_REQUESTS_PER_DAY = 5
+    LENGTH_TOP_REQUESTS_NUMBER_LIST = 8
     time_now = datetime.now()
     number_of_query_list, query_content_list, top_requests_texts = [], [], []
     time_threshold = time_now - timedelta(days=1)
     request_list = RequestModel.objects.filter(timestamp__gte=time_threshold)
     top_requests_texts = create_topics_list_by_time_interval(RequestModel, time_threshold)
     for text in top_requests_texts:
-        number_of_query_list.append(create_time_interval_list_of_counts_for_request(
+        number_of_query_list.append(create_topics_count_list_per_week(
             text=text, request_list=request_list, time_now=time_now))
     json_data = {"day": {"numberOfQuery": number_of_query_list,
                          "queryContent": top_requests_texts}}
-    input_empty_lists_to_request_number_list(number_of_query_list, 5, 8)
-    input_empty_strings_to_query_content_list(top_requests_texts, 5)
+    input_empty_lists_to_request_number_list(number_of_query_list, COUNT_OF_TOP_REQUESTS_PER_DAY,
+                                             LENGTH_TOP_REQUESTS_NUMBER_LIST)
+    input_empty_strings_to_query_content_list(top_requests_texts, COUNT_OF_TOP_REQUESTS_PER_DAY)
+    with open('data.json', 'w') as json_file:
+        json.dump(json_data, json_file, indent=4)
+
+
+@app.task
+def create_top_requests_per_week():
+    from authentication.models import RequestModel
+    COUNT_OF_TOP_REQUESTS_PER_WEEK = 5
+    LENGTH_TOP_REQUESTS_NUMBER_LIST = 7
+    time_now = datetime.now()
+    number_of_query_list, query_content_list, top_requests_texts = [], [], []
+    time_threshold = time_now - timedelta(days=7)
+    request_list = RequestModel.objects.filter(timestamp__gte=time_threshold)
+    top_requests_texts = create_topics_list_by_time_interval(RequestModel, time_threshold)
+    for text in top_requests_texts:
+        number_of_query_list.append(create_topics_count_list_per_week(
+            text=text, request_list=request_list, time_now=time_now))
+    json_data = {"week": {"numberOfQuery": number_of_query_list,
+                          "queryContent": top_requests_texts}}
+    input_empty_lists_to_request_number_list(number_of_query_list, COUNT_OF_TOP_REQUESTS_PER_WEEK,
+                                             LENGTH_TOP_REQUESTS_NUMBER_LIST)
+    input_empty_strings_to_query_content_list(top_requests_texts, COUNT_OF_TOP_REQUESTS_PER_WEEK)
     with open('data.json', 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
 
 
 def create_time_interval_list_of_counts_for_request(text, request_list, time_now):
-    from authentication.models import RequestModel
     THREE_HOURS_AGO_COEF = 1
     ONE_DAY_AGO_COEF = 9
     TIME_INTERVAL = 3
@@ -51,6 +74,18 @@ def create_time_interval_list_of_counts_for_request(text, request_list, time_now
     for time_coef in range(THREE_HOURS_AGO_COEF, ONE_DAY_AGO_COEF):
         time_left = time_now - timedelta(hours=TIME_INTERVAL * time_coef)
         time_right = time_now - timedelta(hours=TIME_INTERVAL * (time_coef - 1))
+        request_list_per_current_interval = request_list.filter(
+            text=text, timestamp__range=[time_left, time_right])
+        number_list.append(request_list_per_current_interval.count())
+    number_list.reverse()
+    return number_list
+
+
+def create_topics_count_list_per_week(text, request_list, time_now):
+    number_list = []
+    for day in range(1, 8):
+        time_left = time_now - timedelta(days=day)
+        time_right = time_now - timedelta(days=day-1)
         request_list_per_current_interval = request_list.filter(
             text=text, timestamp__range=[time_left, time_right])
         number_list.append(request_list_per_current_interval.count())
