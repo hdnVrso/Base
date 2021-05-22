f635300ebdf10bcb84a55d198ac38856396625e2
boyboyjoy <moximmorozov@yandex.ru>
2021-05-22 18:20:47 +0430

    change request model

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,5 @@
 import jwt
-
+from django.utils import timezone
 from datetime import datetime, timedelta
 
 from django.conf import settings
@@ -93,7 +93,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 class RequestModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(blank=False, max_length=100)
-    timestamp = models.DateTimeField(auto_created=True, auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.text
