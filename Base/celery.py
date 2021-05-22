141d9052b6b91e09f98a9c8d7e8f8a1cb63bed18
boyboyjoy <moximmorozov@yandex.ru>
2021-05-22 16:47:46 +0430

    refactor celery functions

--- Base/celery.py
+++ Base/celery.py
@@ -1,5 +1,8 @@
 import os
 from celery import Celery
+from collections import OrderedDict
+from datetime import datetime, timedelta
+import json
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Base.settings")
 
@@ -22,36 +25,25 @@ app.conf.timezone = 'UTC'
 @app.task
 def get_top():
     from authentication.models import RequestModel
-    from collections import OrderedDict
-    from datetime import datetime, timedelta
-    import json
     COUNT_OF_TOP_REQUESTS_PER_DAY = 5
     time_now = datetime.now()
-    top_requests = {}
     number_of_query_list, query_content_list, top_requests_texts = [], [], []
     time_threshold = time_now - timedelta(days=1)
     request_list = RequestModel.objects.filter(timestamp__gte=time_threshold)
-    for request in request_list:
-        top_requests[request.text] = top_requests.get(request.text, 0) + 1
-    sorted_request_dict = (OrderedDict(sorted(top_requests.items(), key=lambda t: t[1])))
-    print(sorted_request_dict)
-    for request in list(sorted_request_dict.items())[-5:]:
-        top_requests_texts.append(request[0])
+    top_requests_texts = create_topics_list_by_time_interval(RequestModel, time_threshold)
     for text in top_requests_texts:
         number_of_query_list.append(create_time_interval_list_of_counts_for_request(
             text=text, request_list=request_list, time_now=time_now))
-    for _ in (range(COUNT_OF_TOP_REQUESTS_PER_DAY - len(number_of_query_list))):
-        number_of_query_list.append([0, 0, 0, 0, 0, 0, 0, 0])
     json_data = {"day": {"numberOfQuery": number_of_query_list,
-                             "queryContent": top_requests_texts}}
+                         "queryContent": top_requests_texts}}
+    input_empty_lists_to_request_number_list(number_of_query_list, COUNT_OF_TOP_REQUESTS_PER_DAY, 8)
+    input_empty_lists_to_query_content_lists(top_requests_texts, COUNT_OF_TOP_REQUESTS_PER_DAY)
     with open('data.json', 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
 
 
 def create_time_interval_list_of_counts_for_request(text, request_list, time_now):
     from authentication.models import RequestModel
-    from collections import OrderedDict
-    from datetime import datetime, timedelta
     THREE_HOURS_AGO_COEF = 1
     ONE_DAY_AGO_COEF = 9
     TIME_INTERVAL = 3
@@ -64,3 +56,24 @@ def create_time_interval_list_of_counts_for_request(text, request_list, time_now
         number_list.append(request_list_per_current_interval.count())
     number_list.reverse()
     return number_list
+
+
+def create_topics_list_by_time_interval(model, time_interval):
+    top_requests = {}
+    top_requests_texts = []
+    for request in model.objects.filter(timestamp__gte=time_interval):
+        top_requests[request.text] = top_requests.get(request.text, 0) + 1
+    sorted_request_dict = (OrderedDict(sorted(top_requests.items(), key=lambda t: t[1])))
+    for request in list(sorted_request_dict.items())[-5:]:
+        top_requests_texts.append(request[0])
+    return top_requests_texts
+
+
+def input_empty_lists_to_request_number_list(request_number_list, list_length, empty_list_length):
+    for _ in (range(list_length - len(request_number_list))):
+        request_number_list.append([0 for i in range(empty_list_length)])
+
+
+def input_empty_lists_to_query_content_lists(query_content_list, list_length):
+    for _ in (range(list_length - len(query_content_list))):
+        request_number_list.append(["" for i in range(empty_list_length)])
