3da4451b522439360011b6272b073d8a92a0b4dc
boyboyjoy <moximmorozov@yandex.ru>
2021-05-23 12:08:59 +0430

    replace request model

--- authentication/models.py
+++ authentication/models.py
@@ -88,12 +88,3 @@ class User(AbstractBaseUser, PermissionsMixin):
         }, settings.SECRET_KEY, algorithm='HS256')
 
         return refresh_token
-
-
-class RequestModel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField(blank=False, max_length=100)
-    timestamp = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.text
