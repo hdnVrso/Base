360c26a7ccf5ec59c45524c0320acf2ffb672be1
boyboyjoy <moximmorozov@yandex.ru>
2021-05-23 17:40:07 +0430

    fix pep8

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from .views import Health, Requests, RequestsHistory, RequestsRating, ResetPassw
 urlpatterns = [
     path('health/', Health.as_view(), name='health'),
     path('celery_data/', CeleryDataView.as_view(), name='celery_data'),
-    #path('requests/', Requests.as_view(), name='requests'),
-    #path(
+    # path('requests/', Requests.as_view(), name='requests'),
+    # path(
     #    'requests/history',
     #    RequestsHistory.as_view(),
     #    name='requests_history'),
-    #path('requests/rating', RequestsRating.as_view(), name='requests_rating'),
-    #path('resetPassword', ResetPassword.as_view(), name='reset_password')
+    # path('requests/rating', RequestsRating.as_view(), name='requests_rating'),
+    # path('resetPassword', ResetPassword.as_view(), name='reset_password')
 ]
